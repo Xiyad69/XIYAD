941220b5b9828d1e718bbdf5cc57ba6e74cd1691
XIYAD <118036348+Xiyad69@users.noreply.github.com>
2023-08-26 02:02:36 +0600

    Create Xiyad.py

---
+++ Xiyad.py
@@ -0,0 +1,30 @@
+#<\>!python3.11
+#<\>coded by YousuF
+#----------------Don't Copy This Script--------------#
+import os, sys, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/Xiyad.XD/')
+import requests
+try:
+    if sys.argv[1]=='update':
+        os.system('rm -rf xd.so')
+except:
+    pass
+os.system('rm -rf xd.so')
+os.system('git pull')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    if not os.path.isfile('xd.so'):
+        os.system('curl -L https://github.com/Xiyad69/XIYAD/blob/main/xd.cpython-311.so?raw=true -o xd.so') 
+        import xd  
+    else:
+        import xd
+elif bit == '32bit':
+    exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+    
+    
+
