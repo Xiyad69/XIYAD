0717d65cc4a6bc800ea6875ab081ab5e4fc9c35a
XIYAD <118036348+Xiyad69@users.noreply.github.com>
2024-03-12 13:22:51 +0600

    Update Xiyad.py

--- Xiyad.py
+++ Xiyad.py
@@ -6,34 +6,26 @@ try:
     import requests
 except:
     os.system('pip install requests')
-#os.system('xdg-open https://chat.whatsapp.com/BbnGtyHZcmZ3F8bDnwwFlG')
-os.system('xdg-open https://facebook.com/Xiyad.404.XD/')
+os.system('xdfileg-open https://facebook.com/Xiyad.4040.XD/')
 import requests
 try:
     if sys.argv[1]=='update':
-        os.system('rm -rf xd.so')
-        os.system('rm -rf xd32.so')
+        os.system('rm -rf xd')
 except:
     pass
-os.system('rm -rf xd.so')
-os.system('rm -rf xd32.so')
+os.system('rm -rf xd')
 os.system('git pull')
 #os.system('clear')
 #exit('\033[91;1mCOMMAND OFF\033[1;37m ')
 bit = platform.architecture()[0]
 if bit == '64bit':
-    if not os.path.isfile('xd.so'):
-        os.system('curl -L https://github.com/Xiyad69/XIYAD/blob/main/xd.cpython-311.so?raw=true -o xd.so') 
-        import xd  
+    if not os.path.isfile('xd'):
+        os.system('curl -L https://github.com/Xiyad69/XIYAD/blob/main/xdfile.cpython-311.so?raw=true -o xd') 
+        import xdfile  
     else:
-        import xd
+        import xdfile
 elif bit == '32bit':
-	if not os.path.isfile(''):
-		os.system('curl -L ') 
-		import xd32  
-	else:
-		import xd32 
-    #exit('\033[1;31m\n Sorry System or 32bit device not supported ')
+    exit('\033[1;31m\n Sorry System or 32bit device not supported ')
     
     
 
